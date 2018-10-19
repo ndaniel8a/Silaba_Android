84911618719bb49cb21c0fbd3a5666b70cf9307c
Nelson Daniel Ochoa Avendano <ndaniel8a@gmail.com>
2018-10-19 17:45:21 -0500

    Definicion de PostService

---
+++ app/src/main/java/co/com/silaba/services/PostService.java
@@ -0,0 +1,34 @@
+package co.com.silaba.services;
+
+public interface PostService {
+  
+  //TODO: init(session: AuthSession)
+  
+  public abstract void 
+  
+  /*
+  
+  
+  func fetchPostInfo(id: String, callback: ((PostServiceResult) -> Void)?)
+  func fetchPosts(userId: String, offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func writePost(photoId: String, content: String, imageReconized: MachineLearningList, talent: Talent, callback: ((PostServiceResult) -> Void)?)
+  
+  func fetchLikes(id: String, offset: String, limit: UInt, callback: ((PostServiceLikeResult) -> Void)?)
+  func like(id: String, callback: ((PostServiceError?) -> Void)?)
+  func unlike(id: String, callback: ((PostServiceError?) -> Void)?)
+  
+  func fetchShares(id: String, offset: String, limit: UInt, callback: ((PostServiceShareResult) -> Void)?)
+  func share(id: String, callback: ((PostServiceError?) -> Void)?)
+  func unshare(id: String, callback: ((PostServiceError?) -> Void)?)
+  func getUserFromUsername(of usersname: [String], callback: (([User]) -> Void)?)
+  
+  func fetchDiscoveryPosts(offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func fetchDiscoveryPosts(recognized: String, talentId: String, offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func fetchLikedPosts(userId: String, offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func fetchRecommendPosts(withRecognized recognized: String, withTalentId talentId: String, offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func fetchRecommendPosts(offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  func fetchTaggedPosts(tag: String,  offset: String, limit: UInt, callback: ((PostServiceResult) -> Void)?)
+  */
+  
+
+}
